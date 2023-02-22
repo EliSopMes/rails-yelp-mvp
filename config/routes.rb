9f95c5c06e8b77c1f7461e788a4359a197e0615e
Elisabeth Messmer <elisabethsophiem@web.de>
2023-02-22 15:32:39 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# collection do
+# get :all
+# get :new
+# post
